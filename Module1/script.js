687507d7c4c50af4b74f38efb25608a5647997d2
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-22 16:07:32 +0600

    some added

---
+++ Module1/script.js
@@ -0,0 +1,18 @@
+// problem 1 : write an arrow function that will take 3 param will multiply the param and will return the reslt 
+const mult= (a,b,c)=>a*b*c;
+const result =mult(2,2,8);
+// console.log(result);
+
+// problem 2 : Write the following sentence in three lines and print the result .i  am web developer . I love to code .I love to eat biriany.
+const sentence =`I am web developer.
+I love to code .
+I love to eat biriyani`;
+// console.log(sentence);
+
+
+// problem 3 :write an arrow function that have a tow parm but one param has a default value and finally  add the valu and return the value;
+const add =(a,b=5)=>a+b;
+const resultAdd =add(1);
+// console.log(resultAdd);
+
+
