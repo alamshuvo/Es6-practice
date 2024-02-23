2996250b54f9d6508dddb821fddfbe5d49011648
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-23 17:17:58 +0600

    another one

--- Module2/practice.js
+++ Module2/practice.js
@@ -79,3 +79,46 @@ const data2={
 }
 console.log(data2.data[0].bookDetails.name);
 console.log(data2.data[1].bookCatagory);
+
+
+// task 4 :;
+//  You have an odd array ,array with odd numbers [1,3,5,7,9]
+// now convert this array into an even array[2,4,6,8,10,12] do this using for loop and array map method hints:add one to any odd numbers and it will become an even number
+const n=[1,3,5,7,9];
+const converEven=n.map(a=>a+1);
+console.log(converEven);
+
+
+
+const numbers=[1,3,5,7,9];
+function number(numbers){
+    let f=[];
+  for (let number of numbers) {
+    number+=1;
+    f.push(number);
+  }
+return f;
+}
+console.log(number(numbers));
+
+
+// task 5 : You are given an array say [33,50,79,78,90,101,30] ,now return get all the elements which are divisiable by 10 using arra.filter  method now do the same task of quenstion 2 but do this using array find method then compare the output of question 2 and 3
+
+// using filter method 
+const numbers1 =[33,50,79,78,90,101,30];
+const divisiableBy=numbers1.filter(n=>{
+    if (n%10==0) {
+        return n;
+    }
+})
+console.log(divisiableBy);
+
+// using find method
+const another=numbers1.find(a=>{
+    if (a%10==0) {
+        return a;
+    }
+})
+console.log(another);
+
+// defferance between array filter method and array find method .array filter method return a new array .that condition  matchd on the other hand find method  return a single number that have first condition matched .Both method are not change old array .
