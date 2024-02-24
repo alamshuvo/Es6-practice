54986d31d50f155d2851e602440dc97e379b9519
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-24 11:38:52 +0600

    tripple

---
+++ Module3/tripple.js
@@ -0,0 +1,10 @@
+// triple equal type soho chek kore takhe .but == equal sadaranoto type check kore na .   1. bitore kisu conversion er kaj hoye takhe .type coersion .type casting .behind the seen hole se bollian e convert kore takhe ==  equal dyea check korle .
+
+const first=false;
+const second=0;
+if (first===second) {
+    console.log("values are equal");
+}
+else{
+    console.log("values are not equal");
+}
