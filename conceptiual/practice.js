613f02deca6c2b400e7d8b34ab29be8d40902bb9
iftakhar <iftakharalamshuvo11@gmail.com>
2024-02-26 12:17:26 +0600

    array spreed

---
+++ conceptiual/practice.js
@@ -0,0 +1,31 @@
+// spreed
+// const numbers=[1,2,3,4];
+// const num1=numbers;
+// num1.push(10);
+// numbers.push(20);
+// console.log("num1 =",num1);
+// console.log("numbers = ",numbers);
+// non premitiive data type tader referance ta k ake oporer sathe share kore tahke and value return korena reference k dore rake .ai osubida dur korar jonno amra spreeding ta use kore takhi .
+// const numbers=[1,2,3,4];
+// let num1=[];
+// for (const num of numbers) {
+// num1.push(num);    
+// }
+// num1.push(10);
+// numbers.push(20)
+// console.log(num1);
+// console.log(numbers);
+// ekahne alada alada value dewar karon holo akhane alada akta empty array crate hoi jar address o alada ,
+
+// const numbers=[1,2,3,4,5];
+// const num=[...numbers];
+// num.push(10);
+// numbers.push(20)
+// console.log(numbers);
+// console.log(num);
+
+
+
+
+
+
